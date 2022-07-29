c57b2eef4d420441241af71c3f28b2534b3107f8
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2022-07-29 12:02:50 +0900

    ☑️ 상품 목록 - 타임리프

---
+++ src/main/java/hello/itemservice/domain/web/basic/BasicItemController.java
@@ -0,0 +1,40 @@
+package hello.itemservice.domain.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+//    @Autowired
+//    public BasicItemController(ItemRepository itemRepository) {
+//        this.itemRepository = itemRepository;
+//    } ->@RequiredArgsConstructor로 생략 가능
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    //for test
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemA", 20000, 20));
+    }
+}
