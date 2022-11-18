e910499aec531d15334f96e073871bd485d3410d
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2022-11-18 14:21:02 +0900

    ☑️ spring을 이용한 세션 관리

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -59,7 +60,7 @@ public class HomeController {
         return "loginHome";
 
     }
-        @GetMapping("/")
+//        @GetMapping("/")
     public String homeLoginV3(HttpServletRequest request, Model model){
 
         HttpSession session = request.getSession(false);
@@ -67,6 +68,18 @@ public class HomeController {
             return "home";
         }
         Member loginMember = (Member) session.getAttribute(SessionConst.LONGIN_MEMBER);
+
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+
+    }
+    @GetMapping("/")
+    public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LONGIN_MEMBER,required = false) Member loginMember, Model model){
+
 
         if (loginMember == null) {
             return "home";
