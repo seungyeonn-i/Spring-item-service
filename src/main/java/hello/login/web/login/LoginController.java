b75dcf07904264e7cbdf885c20ce420d983d4914
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2022-11-18 11:40:08 +0900

    ☑️ 세션을 이용한 로그인/고/로그아웃

--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
@@ -46,7 +47,7 @@ public class LoginController {
         // TODO : 로그인 성공 처리
         // 쿠키 만들기
         // 쿠키에 시간 정보 주지 않으면 세션 쿠키(브라우저 종료시 종료)
-        Cookie idCookie = new Cookie("userId", String.valueOf(loginMember.getId()));
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
         response.addCookie(idCookie);
 
         return "redirect:/";
@@ -76,9 +77,9 @@ public class LoginController {
 
     }
 
-    @PostMapping("/logout")
+//    @PostMapping("/logout")
     public String logout(HttpServletResponse response) {
-        expireCookie(response, "userId");
+        expireCookie(response, "memberId");
         return "redirect:/";
     }
 
@@ -86,5 +87,11 @@ public class LoginController {
         Cookie cookie = new Cookie(cookieName, null);
         cookie.setMaxAge(0);
         response.addCookie(cookie);
+    }
+
+    @PostMapping("/logout")
+    public String logoutV2(HttpServletRequest request) {
+        sessionManager.expire(request);
+        return "redirect:/";
     }
 }
