ca33dfc4428dee87c0c29fa97cfc189d07a232a8
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2022-12-09 16:19:13 +0900

    ☑️ 스프링부트를 이용한 오류 페이지 관리

--- src/main/java/hello/exception/servlet/ServletExceptionController.java
+++ src/main/java/hello/exception/servlet/ServletExceptionController.java
@@ -13,9 +13,13 @@ public class ServletExceptionController {
 
     @GetMapping("/error-ex")
     public void errorEx() {
-        throw new RuntimeException("예외 발생@");
+        throw new RuntimeException("예외 발생!");
     }
 
+    @GetMapping("/error-400")
+    public void error400(HttpServletResponse response) throws IOException {
+        response.sendError(400, "400오류");
+    }
     @GetMapping("/error-404")
     public void error404(HttpServletResponse response) throws IOException {
         response.sendError(404, "404오류");
