4b7d3a5009a4f65babf8ed84a8fb64dc584612cb
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2022-12-12 17:26:19 +0900

    ☑️ exceptionResolver를 사용하면 서블릿 컨테이너까지 예외가 올라가지 않는다

---
+++ src/main/java/hello/exception/resolver/MyHandlerExceptionResolver.java
@@ -0,0 +1,31 @@
+package hello.exception.resolver;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+public class MyHandlerExceptionResolver implements HandlerExceptionResolver {
+    @Override
+    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+
+        // 정상적인 ModelAndView로 변환
+        try {
+
+            if (ex instanceof IllegalArgumentException) {
+                log.info("IllegalArgumentException resolver to 400");
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
+                return new ModelAndView(); // 에외 먹어버림
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            log.error("resolver ex", e);
+        }
+
+        return null;
+    }
+}
